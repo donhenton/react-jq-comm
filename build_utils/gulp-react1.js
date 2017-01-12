99b6c71b8aab5aeac7991a18acb4bee103c05071
Don Henton <donhenton@gmail.com>
2017-01-12 08:33:12 -0600

    setting up gulp 1

--- build_utils/gulp-react1.js
+++ build_utils/gulp-react1.js
@@ -9,13 +9,13 @@ var source = require('vinyl-source-stream');
 var streamify = require('gulp-streamify');
 var gulpif = require("gulp-if");
 var props = require("./build_utils").props;
- 
+var watch = require('gulp-watch');
 
 function Bundle(srcLocation) {
 
     var Bundler = browserify({
         entries: srcLocation,
-        transform: [["babelify", {"presets": ["es2015","react"]}]],
+        transform: [["babelify", {"presets": ["es2015", "react"]}]],
         extensions: ['.js'],
         debug: true,
         cache: {},
@@ -31,11 +31,11 @@ gulp.task('build-react1', function () {
     Bundle('src/code/react1/react1.js')
             .pipe(source('bundle-react1.js'))
             .pipe(gulpif(argv.production, streamify(uglify())))
-       //     .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+            //     .pipe(gulpif(argv.production, rename({suffix: '.min'})))
             .pipe(gulp.dest(props.targetLocation))
             .on('finish', function ( ) {
-                gutil.log("build bundle end");
-                 livereload.reload(props.pageURL);
+                gutil.log("react2 build bundle end");
+                livereload.reload(props.pageURL);
             });
     ;
 });
@@ -44,44 +44,64 @@ gulp.task('build-react2', function () {
     Bundle('src/code/react2/react2.js')
             .pipe(source('bundle-react2.js'))
             .pipe(gulpif(argv.production, streamify(uglify())))
-       //     .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+            //     .pipe(gulpif(argv.production, rename({suffix: '.min'})))
             .pipe(gulp.dest(props.targetLocation))
             .on('finish', function ( ) {
-                gutil.log("build bundle end");
-                 livereload.reload(props.pageURL);
+                gutil.log("react1 build bundle end");
+                livereload.reload(props.pageURL);
             });
     ;
 });
 
 gulp.task('build-jquery', function () {
-    
-    
+
+
     function JQBundle() {
 
-    var Bundler = browserify({
-        entries: './src/code/jquery/jquery1.js',
-        transform: [["babelify", {"presets": ["es2015"]}]],
-        extensions: ['.js'],
-        debug: true,
-        cache: {},
-        packageCache: {},
-        fullPaths: true
-    });
-    return Bundler
-            .bundle()
-            .on('error', notify);
-}
-    
-    
-    
+        var Bundler = browserify({
+            entries: './src/code/jquery/jquery1.js',
+            transform: [["babelify", {"presets": ["es2015"]}]],
+            extensions: ['.js'],
+            debug: true,
+            cache: {},
+            packageCache: {},
+            fullPaths: true
+        });
+        return Bundler
+                .bundle()
+                .on('error', notify);
+    }
+
+
+
     JQBundle()
             .pipe(source('bundle-jq.js'))
             .pipe(gulpif(argv.production, streamify(uglify())))
-       //     .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+            //     .pipe(gulpif(argv.production, rename({suffix: '.min'})))
             .pipe(gulp.dest(props.targetLocation))
             .on('finish', function ( ) {
-                gutil.log("build bundle end");
-                 livereload.reload(props.pageURL);
+                gutil.log("jq build bundle end");
+                livereload.reload(props.pageURL);
             });
     ;
+});
+
+
+gulp.task('watch-code', function () {
+
+    watch(['./src/code/react1/**/*.js'], function (events, done) {
+
+        gulp.start('build-react1');
+    });
+    watch(['./src/code/react2/**/*.js'], function (events, done) {
+
+        gulp.start('build-react2');
+    });
+    watch(['./src/code/jquery/**/*.js'], function (events, done) {
+
+        gulp.start('build-jquery');
+    });
+
+
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -32,9 +32,109 @@ var gulpif = require("gulp-if");
 var argv = require('yargs').argv;
 var rename = require("gulp-rename");
 require('./build_utils/gulp-react1');
-
 var gulp = require('gulp');
+
+
+var SASS_FILES = './src/sass/**/*.scss';
+var WATCH_JS = ['./src/**/*.js'];
+var MAIN_HTML_FILE = ['./src/html/index.html'];
+
+
+gulp.task('serve', function (done) {
+    livereload.listen();
+    gulp.src('target')
+            .pipe(server({
+                livereload: {
+                    enable: true
+                },
+                host: '127.0.0.1',
+                port: 8080,
+                defaultFile: 'index.html',
+                directoryListing: false,
+                open: true
+            }));
+});
 
 gulp.task('default', function () {
     // place code for your default task here
 });
+
+
+gulp.task('copy-html', function () {
+
+
+    // base allows to copy the folders above the file
+    // return gulp.src(MAIN_HTML_FILE,{'cwd': './src/html','base':'./..'} )
+    return gulp.src(MAIN_HTML_FILE).pipe(gulp.dest(props.targetLocation))
+            .on('finish', function ( ) {
+                gutil.log("processing change in html");
+                livereload.reload(props.pageURL);
+               // cb();
+            });
+ 
+
+});
+
+var sassProcess =
+        function () {
+
+            return gulp.src(SASS_FILES)
+                    .pipe(sass().on('error', sass.logError))
+                    .pipe(concat('main.css'))
+                  //  .pipe(uglifycss())
+                    .pipe(gulp.dest(props.targetLocation));
+        };
+
+gulp.task('sass', function () {
+    sassProcess();
+
+});
+
+gulp.task('copy-assets', function () {
+    
+      gulp.src(['./src/html/imgs/**/*'] )
+              .pipe(gulp.dest(props.targetLocation+'/images'));
+       gulp.src(['./src/html/icons/**/*'] )
+              .pipe(gulp.dest(props.targetLocation+'/icons'));
+       gulp.src(['./src/html/**/*.css'] )
+              .pipe(gulp.dest(props.targetLocation));
+  
+    
+});
+
+
+gulp.task('watch-general', function () {
+
+    watch(SASS_FILES, function (events, done) {
+
+        sassProcess()
+                .on('finish', function ( ) {
+                    gutil.log("processing change in css");
+                    livereload.reload(props.pageURL);
+                });
+
+    });
+
+     
+
+    watch(MAIN_HTML_FILE, function (events, done) {
+        gutil.log("starting html change");
+        gulp.start('copy-html');
+    });
+
+});
+
+
+
+gulp.task('clean', function (  ) {
+
+    del(['target']);
+
+});
+
+ 
+
+gulp.task('build', ['build-react1','build-react2','build-jquery']);
+
+gulp.task('release', gulpsync.sync(['clean','build', 'sass']));
+gulp.task('dev', gulpsync.sync(['clean', 'build', 'sass','copy-assets', 'copy-html', 'watch-general', 'watch-code', 'serve']));
